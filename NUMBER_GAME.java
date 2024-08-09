2207b7bd27945ae457e149fc05dd2d95857cfe6a
Suraj Srivastav <118558931+surajsri23@users.noreply.github.com>
2024-08-09 15:34:40 +0530

    Upload NUMBER_GAME.java

--- NUMBER_GAME.java
+++ NUMBER_GAME.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class NumberGuessingGame {
+public class NUMBER_GAME {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
